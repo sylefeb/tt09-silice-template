bd33a6356a29e78333011e4843e39273a2da88dc
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2024-09-07 19:17:02 +0200

    first attempt at integrating silice into the GDS workflow

--- test/tb.v
+++ test/tb.v
@@ -23,8 +23,8 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  // Replace tt_um_silice with your module name:
+  tt_um_silice user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
